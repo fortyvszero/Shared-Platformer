79d1969406836a0fd1590cf4ee50bcce4ba01f7e
FortyVsZero <adrian.lote@gmail.com>
2024-09-01 14:29:30 +0100

    Basic moving enemies

--- game/enemies/enemy.gd
+++ game/enemies/enemy.gd
@@ -3,12 +3,16 @@ extends CharacterBody2D
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var shader_player: AnimationPlayer = $ShaderPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var movable_ground_cast: RayCast2D = $MovableGroundCast
 
 @export var max_health := 3
 var current_health
 
-const SPEED = 300.0
+const SPEED = 2000.0
 const JUMP_VELOCITY = -400.0
+
+var moveDir = Vector2.RIGHT
 
 func _ready() -> void:
 	current_health = max_health
@@ -34,8 +38,15 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	move_and_slide()
+	velocity.x = SPEED * moveDir.x * delta
+	
+	print(velocity)
+	var collided = move_and_slide()
 
+	if is_on_wall() or !movable_ground_cast.is_colliding():
+		moveDir = -moveDir
+		sprite_2d.flip_h = !sprite_2d.flip_h
+		movable_ground_cast.target_position.x = -movable_ground_cast.target_position.x
 
 
 func _on_shader_player_animation_finished(anim_name: StringName) -> void:
